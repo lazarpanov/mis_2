9fe4cbd8b0cb02f8efdd7254601bb300c7f9c99d
Lazar Panov <lazarpanov2002@gmail.com>
2024-12-21 03:19:57 +0100

    working partly

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../services/api_services.dart';
 import 'joke_list_screen.dart';
 import 'random_joke_screen.dart';
+import 'favorite_jokes_screen.dart';
+import '../models/joke.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -11,6 +13,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   final ApiService apiService = ApiService();
   List<String> jokeTypes = [];
+  List<Joke> favoriteJokes = []; // List to store favorite jokes
 
   @override
   void initState() {
@@ -29,21 +32,49 @@ class _MainScreenState extends State<MainScreen> {
     }
   }
 
+  void addToFavorites(Joke joke) {
+    setState(() {
+      if (!favoriteJokes.contains(joke)) {
+        favoriteJokes.add(joke);
+      }
+    });
+  }
+
+  void removeFromFavorites(Joke joke) {
+    setState(() {
+      favoriteJokes.remove(joke);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Joke types'),
+        title: Text('Joke Categories'),
         actions: [
           IconButton(
-            icon: Icon(Icons.sentiment_very_satisfied),
+            icon: Icon(Icons.lightbulb_outline),
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (_) => RandomJokeScreen()),
               );
             },
-          )
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => FavoriteJokesScreen(
+                    favoriteJokes: favoriteJokes,
+                    removeFromFavorites: removeFromFavorites,
+                  ),
+                ),
+              );
+            },
+          ),
         ],
       ),
       body: jokeTypes.isEmpty
@@ -62,11 +93,16 @@ class _MainScreenState extends State<MainScreen> {
                 style: Theme.of(context).textTheme.bodyLarge,
               ),
               trailing: Icon(Icons.arrow_forward, color: Colors.teal),
-              onTap: () {
+              onTap: () async {
+                final jokes = await apiService.getJokesByType(type);
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (_) => JokeListScreen(type: type),
+                    builder: (_) => JokeListScreen(
+                      type: type,
+                      jokes: jokes,
+                      addToFavorites: addToFavorites,
+                    ),
                   ),
                 );
               },
